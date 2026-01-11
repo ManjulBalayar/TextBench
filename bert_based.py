fb9bd4d7d7f71bc294d6bde7479ed3d9e21fe6d5
ManjulBalayar <90213041+ManjulBalayar@users.noreply.github.com>
2026-01-11 07:35:20 -0600

    BERT script

---
+++ bert_based.py
@@ -0,0 +1,67 @@
+"""
+In this file, I will be utilizing BERT to generate tokens for my given input sequence + utilizing
+the model that I've trained to generate sentiment analysis classification on the text input. 
+"""
+from transformers import AutoTokenizer
+from transformers import BertModel
+import torch
+from torch import nn, optim
+from torch.utils import data
+
+BERT_MODEL = "bert-base-uncased"
+
+def bert_tokenizer(text):
+	tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL)
+	tokens = tokenizer.tokenize(text)
+	print(tokens)
+
+class BERTSentimentClassifier(nn.Module):
+	
+	def __init__(self, n_classes):
+		super(BERTSentimentClassifier, self).__init__()
+		self.bert = BertModel.from_pretrained(BERT_MODEL)
+		self.drop = nn.Dropout(p=0.30)
+		self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+		self.softmax = nn.Softmax(dim=1)
+
+	def forward(self, input_ids, attention_mask):
+		outputs = self.bert(
+			input_ids=input_ids,
+			attention_mask=attention_mask,
+		)
+		pooled_output = outputs.pooler_output
+		output = self.drop(pooled_output)
+		output = self.out(output)
+		return output
+
+class_names = ['negative', 'neutral', 'positive']
+model = BERTSentimentClassifier(len(class_names))
+model.load_state_dict(torch.load('models/bert-reviews.bin', map_location=torch.device('cpu')))
+
+model.eval()
+
+text = "The place was very clean and the food was great! Staff was really nice as well!"
+bert_tokenizer(text)
+print()
+
+tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL)
+encoded_review = tokenizer.encode_plus(
+	text,
+	max_length=128,
+	add_special_tokens=True,
+	padding='max_length',
+	truncation=True,
+	return_attention_mask=True,
+	return_token_type_ids=False,
+	return_tensors='pt'
+)
+
+input_ids = encoded_review['input_ids']
+attention_mask = encoded_review['attention_mask']
+
+with torch.inference_mode():
+	output = model(input_ids, attention_mask)
+	_, pred = torch.max(output, dim=1)
+
+print(f'Text: {text}')
+print(f'Sentiment: {class_names[pred.item()]}')
--- semantics.py
+++
@@ -1,5 +0,0 @@
-"""
-In this file, I work with perform data preprocessing using popular ML libraries (unlike the other ones which were rule-based) and I also work with BERT
-to capture the files/texts semantics. Will do similar things such as understanding the sentiment of the text like regex and compare those two, topic classfication,
-and finally a summarization which rule-based cannot do.
-"""
