1738e8fe70cb97b75e7208db6b88f829da637bec
ManjulBalayar <90213041+ManjulBalayar@users.noreply.github.com>
2026-01-13 22:18:05 -0600

    Repo page addition

---
+++ webapp/pages/4_Repo.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title("GitHub Repository")
+
+st.write("You can find the code for this project on my GitHub repository: [TextBench](https://github.com/ManjulBalayar/TextBench.git)")
+
+st.divider()
+
+st.write("The dataset used in this project is from the Google Local dataset: [Google Local](https://mcauleylab.ucsd.edu/public_datasets/gdrive/googlelocal/#complete-data)")
+
+st.write("The dataset contains 12,000 reviews with a rating from 1 to 5.")
+
+st.divider()
+
+st.write("The BERT model used in this project is from the Hugging Face Transformers library: [BERT](https://huggingface.co/bert-base-uncased)")
+
+st.write("You can find the code for the BERT model on my GitHub repository: [BERT](https://github.com/ManjulBalayar/TextBench/blob/main/notebooks/BERT_Reviews.ipynb)")
+
+st.divider()
+
+st.write("The LLM model used in this project is from the Gemini API: [Gemini](https://cloud.google.com/gemini-api)")
+
+st.write("You can find the code for the LLM model on my GitHub repository: [LLM](https://github.com/ManjulBalayar/TextBench/blob/main/notebooks/LLM_Rule_Based_Reviews.ipynb)")
+
+st.divider()
+
+st.write("The rule-based model used in this project is from the Python re library: [Rule-Based](https://docs.python.org/3/library/re.html)")
+
+st.write("You can find the code for the rule-based model on my GitHub repository: [Rule-Based](https://github.com/ManjulBalayar/TextBench/blob/main/src/rule_based.py)")
